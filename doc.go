3524805d959f6161abac2303a0db01f83dd4bead
Aleksander Korelskiy <alexander.k@devim.team>
2017-09-28 18:21:41 +0300

    Golang structs generating added, range to collect renamed

--- doc.go
+++ doc.go
@@ -1,4 +1,7 @@
 //go:generate protoc --go_out=plugins=grpc:./../../../ -Isrc/main/protobuf src/main/protobuf/devim/protobuf/page.proto
 //go:generate protoc --go_out=plugins=grpc:./../../../ -Isrc/main/protobuf src/main/protobuf/devim/protobuf/time.proto
 //go:generate protoc --go_out=plugins=grpc:./../../../ -Isrc/main/protobuf src/main/protobuf/devim/protobuf/relay.proto
+//go:generate protoc --go_out=plugins=grpc:./../../../ -Isrc/main/protobuf src/main/protobuf/devim/protobuf/dadata.proto
+//go:generate protoc --go_out=plugins=grpc:./../../../ -Isrc/main/protobuf src/main/protobuf/devim/protobuf/collect.proto
+//go:generate protoc --go_out=plugins=grpc:./../../../ -Isrc/main/protobuf src/main/protobuf/devim/protobuf/person.proto
 package proto_utils
