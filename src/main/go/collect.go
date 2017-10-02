0a93894afdbe99eabef639b72124bd459908e4a5
Artyom Blagov <artyom.blagov@devim.team>
2017-10-02 12:55:01 +0300

    fix(collect.go): fix types names

--- src/main/go/collect.go
+++ src/main/go/collect.go
@@ -13,28 +13,28 @@ type Uint64Range struct {
 	End   *uint64
 }
 
-func Uint32RangeToProto(r *Uint32Range) *collect.Uint32Range {
-	return &collect.Uint32Range{
+func Uint32RangeToProto(r *Uint32Range) *collect.UInt32Range {
+	return &collect.UInt32Range{
 		Start: NilUInt32ToProto(r.Start),
 		End:   NilUInt32ToProto(r.End),
 	}
 }
 
-func ProtoToUint32Range(r *collect.Uint32Range) *Uint32Range {
+func ProtoToUint32Range(r *collect.UInt32Range) *Uint32Range {
 	return &Uint32Range{
 		Start: ProtoToNilUInt32(r.Start),
 		End:   ProtoToNilUInt32(r.End),
 	}
 }
 
-func Uint64RangeToProto(r *Uint64Range) *collect.Uint64Range {
-	return &collect.Uint64Range{
+func Uint64RangeToProto(r *Uint64Range) *collect.UInt64Range {
+	return &collect.UInt64Range{
 		Start: NilUInt64ToProto(r.Start),
 		End:   NilUInt64ToProto(r.End),
 	}
 }
 
-func ProtoToUint64Range(r *collect.Uint64Range) *Uint64Range {
+func ProtoToUint64Range(r *collect.UInt64Range) *Uint64Range {
 	return &Uint64Range{
 		Start: ProtoToNilUInt64(r.Start),
 		End:   ProtoToNilUInt64(r.End),
